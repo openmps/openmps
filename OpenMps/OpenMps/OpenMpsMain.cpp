4bbbb2d0012fda78d34cd102412dfa9b9938f911
aokomoriuta <internal@enmps.net>
2014-10-02 18:52:14 +0900

    不要コードの除去

--- OpenMps/OpenMps/OpenMpsMain.cpp
+++ OpenMps/OpenMps/OpenMpsMain.cpp
@@ -91,14 +91,11 @@ static OpenMps::Particle::List CreateParticles(const double l_0, const double co
 				double u = make_rand()*courant;
 				double v = make_rand()*courant;
 
-				auto particle = std::shared_ptr<Particle>(new ParticleIncompressibleNewton(x, y, u, v, 0, 0));
+				std::shared_ptr<Particle> particle(new ParticleIncompressibleNewton(x, y, u, v, 0, 0));
 				particles.push_back(particle);
 			}
 		}
 		
-		auto particle2 = std::shared_ptr<Particle>(new ParticleIncompressibleNewton(l_0*0.3, 0, 0, 0, 0, 0));
-		//particles.push_back(particle2);
-
 		// 床を追加
 		for(int i = -1; i < L+1; i++)
 		{
@@ -107,10 +104,10 @@ static OpenMps::Particle::List CreateParticles(const double l_0, const double co
 			// 床
 			{
 				// 粒子を作成して追加
-				auto wall1 = std::shared_ptr<Particle>(new ParticleWall(x, -l_0*1, 0, 0));
-				auto dummy1 = std::shared_ptr<Particle>(new ParticleDummy(x, -l_0*2));
-				auto dummy2 = std::shared_ptr<Particle>(new ParticleDummy(x, -l_0*3));
-				auto dummy3 = std::shared_ptr<Particle>(new ParticleDummy(x, -l_0*4));
+				std::shared_ptr<Particle> wall1(new ParticleWall(x, -l_0*1, 0, 0));
+				std::shared_ptr<Particle> dummy1(new ParticleDummy(x, -l_0*2));
+				std::shared_ptr<Particle> dummy2(new ParticleDummy(x, -l_0*3));
+				std::shared_ptr<Particle> dummy3(new ParticleDummy(x, -l_0*4));
 				particles.push_back(wall1);
 				particles.push_back(dummy1);
 				particles.push_back(dummy2);
@@ -126,10 +123,10 @@ static OpenMps::Particle::List CreateParticles(const double l_0, const double co
 			// 左壁
 			{
 				// 粒子を作成して追加
-				auto wall1 = std::shared_ptr<Particle>(new ParticleWall(-l_0*1, y, 0, 0));
-				auto dummy1 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*2, y));
-				auto dummy2 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*3, y));
-				auto dummy3 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*4, y));
+				std::shared_ptr<Particle> wall1(new ParticleWall(-l_0*1, y, 0, 0));
+				std::shared_ptr<Particle> dummy1(new ParticleDummy(-l_0*2, y));
+				std::shared_ptr<Particle> dummy2(new ParticleDummy(-l_0*3, y));
+				std::shared_ptr<Particle> dummy3(new ParticleDummy(-l_0*4, y));
 				particles.push_back(wall1);
 				particles.push_back(dummy1);
 				particles.push_back(dummy2);
@@ -146,9 +143,9 @@ static OpenMps::Particle::List CreateParticles(const double l_0, const double co
 			// 左下
 			{
 				// 粒子を作成して追加
-				auto dummy1 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*2, y-4*l_0));
-				auto dummy2 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*3, y-4*l_0));
-				auto dummy3 = std::shared_ptr<Particle>(new ParticleDummy(-l_0*4, y-4*l_0));
+				std::shared_ptr<Particle> dummy1(new ParticleDummy(-l_0*2, y-4*l_0));
+				std::shared_ptr<Particle> dummy2(new ParticleDummy(-l_0*3, y-4*l_0));
+				std::shared_ptr<Particle> dummy3(new ParticleDummy(-l_0*4, y-4*l_0));
 				particles.push_back(dummy1);
 				particles.push_back(dummy2);
 				particles.push_back(dummy3);
@@ -192,7 +189,7 @@ int main()
 	// 開始時間を保存
 	boost::timer timer;
 	timer.restart();
-	auto timeFormat = boost::format("#%3$05d: t=%1$8.4lf (%2$05d) @ %4$02d/%5$02d %6$02d:%7$02d:%8$02d (%9$8.2lf)");
+	boost::format timeFormat("#%3$05d: t=%1$8.4lf (%2$05d) @ %4$02d/%5$02d %6$02d:%7$02d:%8$02d (%9$8.2lf)");
 
 	// 開始時間を画面表示
 	auto t = std::time(nullptr);
