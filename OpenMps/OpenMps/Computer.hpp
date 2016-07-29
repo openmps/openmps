65560a55467740a3e881eb20fdb0311f57a59554
aokomoriuta <internal@enmps.net>
2016-07-29 21:25:17 +0900

    陽的な力の計算部分もOpenMPで並列化

--- OpenMps/OpenMps/Computer.hpp
+++ OpenMps/OpenMps/Computer.hpp
@@ -666,8 +666,15 @@ namespace OpenMps
 
 			// 全粒子で
 			const auto n = particles.size();
-			for(auto i = decltype(n)(0); i < n; i++)
+#ifdef _OPENMP
+#pragma omp parallel for
+			for (auto ii = std::make_signed_t<decltype(n)>(0); ii < static_cast<std::make_signed_t<decltype(n)>>(n); ii++)
 			{
+				const auto i = static_cast<decltype(n)>(ii);
+#else
+			for (auto i = decltype(n)(0); i < n; i++)
+			{
+#endif
 				auto& particle = particles[i];
 
 				// 水粒子のみ
