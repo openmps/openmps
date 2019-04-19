6f623c8be5ec49016d216614845401fd0f84fc05
LWisteria <lwisteria.ao@gmail.com>
2019-04-19 13:53:04 +0900

    ディレクトリ全体を実行するようにした

--- Benchmark/CentralGravity/check_result.py
+++ Benchmark/CentralGravity/check_result.py
@@ -2,19 +2,32 @@
 #-*- coding:utf-8 -*-
 
 import sys
+import os
 import csv
 import math
 import numpy
 from matplotlib import pyplot
 
-def main(filename):
-	with open(filename, "r") as f:
+def output(dirname, filename):
+	with open(dirname + "/" + filename, "r") as f:
 		data = [[math.sqrt(float(line["x"])**2 + float(line["z"])**2), float(line["p"])] for line in csv.DictReader(f, skipinitialspace="True")]
 
 	data = numpy.array(data).T
+	pyplot.xlim([0, 0.06])
+	pyplot.ylim([0, 600])
+	pyplot.xlabel("r [m]")
+	pyplot.ylabel("p [Pa]")
+	pyplot.grid(which="minor", color="lightgray", linestyle="-")
+	pyplot.grid(which="major", color="black", linestyle="-", b=True)
+	pyplot.minorticks_on()
 	pyplot.plot(data[0], data[1], ".")
-	pyplot.savefig("out.svg")
+	pyplot.savefig("output/" + filename + ".svg")
 	pyplot.clf()
+
+def main(dirname):
+	for filename in sorted(os.listdir(dirname)):
+		output(dirname, filename)
+		print(filename)
 
 if __name__ == "__main__":
 	main(sys.argv[1])
