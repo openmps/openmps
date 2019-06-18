8da81c2c468c81956c40f4538aae5bbdc9052df7
takafumi MARUISHI <maruishi.takafumi.576@s.kyushu-u.ac.jp>
2019-06-18 10:10:06 +0900

    列優先->行優先 と変更

--- src/OpenMps/test/Computer_test.cpp
+++ src/OpenMps/test/Computer_test.cpp
@@ -33,8 +33,8 @@ TEST(MatrixTest, InvertMatrixDiag){
 TEST(MatrixTest, InvertMatrixDiag2){
 	// 2成分のみ存在 その2
 	// M            invM
-	// [ 0, 2 ]     [  0, 1 ]
-	// [ 1, 0 ]     [ 1/2, 0 ]
+	// [ 0, 1 ]     [0, 1/2]
+	// [ 2, 0 ]     [1, 0  ]
 	const double Ms[] = {0.0,1.0,2.0,0.0};
 	const double iMs[] = {0.0,1.0/2.0,1.0,0.0};
 
@@ -50,8 +50,8 @@ TEST(MatrixTest, InvertMatrixAsym){
 	// 非対称的
   // 全成分入り, 対称性低そうな行列
   // M            invM
-  // [ 1, 3 ]     [-2, 3/2]
-  // [ 2, 4 ]     [ 1, -1/2]
+  // [ 1, 2 ]     [-2,   1 ]
+  // [ 3, 4 ]     [3/2,-1/2]
 	const double Ms[] = {1.0,2.0,3.0,4.0};
 	const double iMs[] = {-2.0,1.0,3./2.,-1./2.};
 
