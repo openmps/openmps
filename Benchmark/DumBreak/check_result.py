dcd7a13edc6f58e8878edc711b443fca4adae597
LWisteria <lwisteria.ao@gmail.com>
2019-06-19 17:58:39 +0900

    +元論文の結果と比較

--- Benchmark/DumBreak/check_result.py
+++ Benchmark/DumBreak/check_result.py
@@ -31,17 +31,47 @@ def output(dirname, filename):
 	return [edge]
 
 def main(dirname, L, dt, g):
+	# Martin & Moyce(1952), 1.125in
+	with open("martin_moyce_1952_1125.csv") as f:
+		data = [[float(line["t"]), float(line["z"])] for line in csv.DictReader(f)]
+	data = numpy.array(data).T
+	pyplot.plot(data[0], data[1], "o", label="Exp: Martin & Moyce(1952), 1.125in")
+
+	# Martin & Moyce(1952), 2.25in
+	with open("martin_moyce_1952_225.csv") as f:
+		data = [[float(line["t"]), float(line["z"])] for line in csv.DictReader(f)]
+	data = numpy.array(data).T
+	pyplot.plot(data[0], data[1], "o", label="Exp: Martin & Moyce(1952), 2.25in")
+
+	# Koshizuka et al.(1995)
+	with open("koshizuka_etal_1995.csv") as f:
+		data = [[float(line["t"]), float(line["z"])] for line in csv.DictReader(f)]
+	data = numpy.array(data).T
+	pyplot.plot(data[0], data[1], "s", label="Exp: Koshizuka et al.(1995)")
+
+	# Hirt & Nichols(1981)
+	with open("hirt_nichols_1981.csv") as f:
+		data = [[float(line["t"]), float(line["z"])] for line in csv.DictReader(f)]
+	data = numpy.array(data).T
+	pyplot.plot(data[0], data[1], "--", label="SOVA-VOF: Hirt & Nichols(1981)")
+
+	# Koshizuka & Oka(1996)
+	with open("koshizuka_oka_1996.csv") as f:
+		data = [[float(line["t"]), float(line["z"])] for line in csv.DictReader(f)]
+	data = numpy.array(data).T
+	pyplot.plot(data[0], data[1], "-", label="Origianl MPS: Koshizuka & Oka(1996)")
+
+	# OpenMPS
 	data = joblib.Parallel(n_jobs=-1, verbose=1)([joblib.delayed(output)(dirname, filename) for filename in sorted(os.listdir(dirname))])
 #	data = [output(dirname, filename) for filename in sorted(os.listdir(dirname)) if filename.startswith("particles_")]
-
 	data = numpy.array(data).T
 	n = data.shape[1]
 	maxT = n * dt
 	t = numpy.array(range(n)) * dt
-
 	tt = math.sqrt(2*g/L)
 	z = data[0]
-	pyplot.plot(t*tt, z/L)
+	pyplot.plot(t*tt, z/L, label="OpenMPS", color="red", linewidth=3)
+
 	pyplot.xlim([0, 3.5])
 	pyplot.ylim([1, 4])
 	pyplot.xlabel("$t\sqrt{2g/L}$")
@@ -49,6 +79,7 @@ def main(dirname, L, dt, g):
 	pyplot.grid(which="minor", color="gray", linestyle="dashed")
 	pyplot.grid(which="major", color="black", linestyle="solid", b=True)
 	pyplot.minorticks_on()
+	pyplot.legend(prop={'size': 10})
 	pyplot.savefig("edge.svg")
 	pyplot.clf()
 
