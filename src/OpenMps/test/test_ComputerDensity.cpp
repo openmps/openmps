4faa406526ba044128eac09ce49e966956aadb02
takafumi MARUISHI <maruishi.takafumi.576@s.kyushu-u.ac.jp>
2019-07-26 09:24:00 +0900

    WIP, add test codes for constructor of environment class

--- src/OpenMps/test/test_ComputerDensity.cpp
+++ src/OpenMps/test/test_ComputerDensity.cpp
@@ -8,8 +8,42 @@
 #include "../Computer.hpp"
 #include "../Particle.hpp"
 #include "../ComputingCondition.hpp"
+#include <iostream>
 
 namespace{
+#ifndef PRESSURE_EXPLICIT
+				const double eps = 1e-10;
+#endif
+#ifdef ARTIFICIAL_COLLISION_FORCE
+				const double tooNearRatio = 1.0;
+				const double tooNearCoefficient = 1.0;
+#endif
+
+				const double startTime = 0.0;
+				const double endTime = 0.5;
+				const double outputInterval = 1.0;
+				const std::size_t minStepCountPerOutput = 10;
+				const double maxDt = outputInterval / minStepCountPerOutput;
+				const double courant = 0.1;
+
+				const double l0 = 0.001;
+				const double g = 9.8;
+				// 計算ステップから指定した時間スケール, dt = maxDt = 0.100
+				// 重力加速度による時間スケール, dt = sqrt(2*l0/g) = 0.0143..
+
+				const double rho = 998.2;
+				const double nu = 1.004e-06;
+				const double r_eByl_0 = 2.4;
+				const double surfaceRatio = 0.95;
+				const double minX = -0.004;
+				const double minZ = -0.004;
+				const double maxX = 0.053;
+				const double maxZ = 0.1;
+
+#ifdef PRESSURE_EXPLICIT
+					const double c = 1.0;
+#endif
+
 	namespace OpenMps{
 	double positionWall(std::size_t, double, double){
 		return 0.0;
@@ -25,39 +59,8 @@ namespace{
 	{
 		protected:
 			OpenMps::Computer<POS_WALL,POS_WALL_PRE> *computer;
-
 			// それぞれのテストケースTEST_Fが呼ばれる直前にSetUpで初期化される
 			virtual void SetUp(){
-				printf("Setup\n");
-
-#ifndef PRESSURE_EXPLICIT
-				const double eps = 1e-10;
-#endif
-#ifdef ARTIFICIAL_COLLISION_FORCE
-				const double tooNearRatio = 1.0;
-				const double tooNearCoefficient = 1.0;
-#endif
-
-				const double startTime = 0.0;
-				const double endTime = 0.5;
-				const double outputInterval = 0.0;
-				const std::size_t minStepCountPerOutput = 100;
-				const double courant = 0.1;
-				const double l0 = 0.001;
-
-				const double g = 9.8;
-				const double rho = 998.2;
-				const double nu = 1.004e-06;
-				const double r_eByl_0 = 2.4;
-				const double surfaceRatio = 0.95;
-				const double minX = -0.004;
-				const double minZ = -0.004;
-				const double maxX = 0.053;
-				const double maxZ = 0.1;
-
-#ifdef PRESSURE_EXPLICIT
-	const double c = 1.0;
-#endif
 
 		auto&& condition = OpenMps::ComputingCondition(
 #ifndef PRESSURE_EXPLICIT
@@ -67,7 +70,7 @@ namespace{
 				outputInterval
 				);
 
-		auto&& environment = OpenMps::Environment(outputInterval / minStepCountPerOutput, courant,
+		auto&& environment = OpenMps::Environment(maxDt, courant,
 #ifdef ARTIFICIAL_COLLISION_FORCE
 			tooNearRatio, tooNearCoefficient,
 #endif
@@ -88,57 +91,51 @@ namespace{
 			positionWall, positionWallPre);
 
 			computer = new OpenMps::Computer<POS_WALL,POS_WALL_PRE>(std::move(comp));
-			}
 
-			virtual void TearDown(){
-				printf("TearDown\n");
-				delete computer;
-			}
+		}
 
-			void searchNeighbor(){
-				computer->SearchNeighbor();
-			}
-
-			void computeNeighborDensities(){
-				computer->ComputeNeighborDensities();
-			}
+		virtual void TearDown(){
+			delete computer;
+		}
   };
 
-	TEST_F(DensityTest,calcDensity){
-		std::vector<OpenMps::Particle> particles;
+	TEST_F(DensityTest, EnvConstructor){
+		auto env = computer->GetEnvironment();
 
-		// 粒子を作成して入れていく
-		const double l0 = 0.1*1e-3; // 0.1 mm
-//		const double re = 2.1*l0;
-		const int nx = 7;
-		const int ny = 7;
+		// Environment TODO: PRESSURE_EXPLICITのifdef
+		const double dt_grav = std::sqrt(2.0*l0/g);
+		ASSERT_LE( env.MaxDt, dt_grav ); // PRESSURE_EXPLICITで分岐必要
 
-		// 1辺l0, nx*nyの正方格子状に粒子を配置
-		for(int j = 0; j < ny; ++j){
-			for(int i = 0; i < nx; ++i){
-				auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
-				particle.X()[OpenMps::AXIS_X] = i*l0;
-				particle.X()[OpenMps::AXIS_Z] = j*l0;;
-
-				particle.U()[OpenMps::AXIS_X] = 0.0;
-				particle.U()[OpenMps::AXIS_Z] = 0.0;
-				particle.P() = 0.0;
-				particle.N() = 0.0;
-
-				particles.push_back(std::move(particle));
-			}
-		}
-		computer->AddParticles(std::move(particles));
-
-//				computer.ForwardTime();
-		searchNeighbor();
-		computeNeighborDensities();
-
-		// 24番目の粒子を狙う
-		// 粒子数を表示
-		std::cout << particles.size() << " particles" << std::endl;
-
-	}
+		ASSERT_DOUBLE_EQ( env.MaxDx, l0*courant ); // PRESSURE_EXPLICITで分岐必要
+		ASSERT_DOUBLE_EQ( env.L_0, l0 );
+		ASSERT_DOUBLE_EQ( env.R_e, r_eByl_0*l0 );
+		ASSERT_DOUBLE_EQ( env.G[0], 0.0 );
+		ASSERT_DOUBLE_EQ( env.G[1], -g );
+		ASSERT_DOUBLE_EQ( env.Rho, rho );
+		ASSERT_DOUBLE_EQ( env.Nu, nu );
+		ASSERT_DOUBLE_EQ( env.SurfaceRatio, surfaceRatio );
+		ASSERT_DOUBLE_EQ( env.MinX[0], minX );
+		ASSERT_DOUBLE_EQ( env.MinX[1], minZ );
+		ASSERT_DOUBLE_EQ( env.MaxX[0], maxX );
+		ASSERT_DOUBLE_EQ( env.MaxX[1], maxZ );
+		ASSERT_GE( env.NeighborLength, r_eByl_0*l0 * (1+ courant*2) ); // 近傍粒子として保持する距離が、クーラン数による距離の二倍以上か？
 	}
 
-}
+	}//openmps
+
+}//anonymas
+
+	///#ifndef PRESSURE_EXPLICIT
+	///				const double eps = 1e-10;
+	///					const double c = 1.0;
+	/////		const double C; // env
+	///#endif
+	///#ifdef ARTIFICIAL_COLLISION_FORCE
+	///				const double tooNearRatio = 1.0;
+	///				const double tooNearCoefficient = 1.0;
+	///#endif
+	///
+	///				const double startTime = 0.0;
+	///				const double endTime = 0.5;
+	///				const double outputInterval = 0.0;
+	///				const std::size_t minStepCountPerOutput = 100;
