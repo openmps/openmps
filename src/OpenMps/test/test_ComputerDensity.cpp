eef65875b45d1521d9ad08c554380b2ad3d13e06
takafumi MARUISHI <maruishi.takafumi.576@s.kyushu-u.ac.jp>
2019-07-26 10:38:46 +0900

    add test cases for constructor of Particles

--- src/OpenMps/test/test_ComputerDensity.cpp
+++ src/OpenMps/test/test_ComputerDensity.cpp
@@ -40,6 +40,10 @@ namespace{
 				const double maxX = 0.053;
 				const double maxZ = 0.1;
 
+				// 格子状に配置する際の1辺あたりの粒子数
+			const int num_ps_x = 7;
+			const int num_ps_z = 7;
+
 #ifdef PRESSURE_EXPLICIT
 					const double c = 1.0;
 #endif
@@ -92,6 +96,26 @@ namespace{
 
 			computer = new OpenMps::Computer<POS_WALL,POS_WALL_PRE>(std::move(comp));
 
+			// 粒子配置
+			std::vector<OpenMps::Particle> particles;
+
+			// 1辺l0, nx*nyの正方格子状に粒子を配置
+			for(int j = 0; j < num_ps_z; ++j){
+				for(int i = 0; i < num_ps_x; ++i){
+					auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
+					particle.X()[OpenMps::AXIS_X] = i*l0;
+					particle.X()[OpenMps::AXIS_Z] = j*l0;;
+
+					particle.U()[OpenMps::AXIS_X] = 0.0;
+					particle.U()[OpenMps::AXIS_Z] = 0.0;
+					particle.P() = 0.0;
+					particle.N() = 0.0;
+
+					particles.push_back(std::move(particle));
+				}
+			}
+			computer->AddParticles(std::move(particles));
+
 		}
 
 		virtual void TearDown(){
@@ -99,7 +123,8 @@ namespace{
 		}
   };
 
-	TEST_F(DensityTest, EnvConstructor){
+	// アクセッサが適切か/フィールドに設定されているか for Environment
+	TEST_F(DensityTest, FieldEnvironment){
 		auto env = computer->GetEnvironment();
 
 		// Environment TODO: PRESSURE_EXPLICITのifdef
@@ -109,20 +134,38 @@ namespace{
 		ASSERT_DOUBLE_EQ( env.MaxDx, l0*courant ); // PRESSURE_EXPLICITで分岐必要
 		ASSERT_DOUBLE_EQ( env.L_0, l0 );
 		ASSERT_DOUBLE_EQ( env.R_e, r_eByl_0*l0 );
-		ASSERT_DOUBLE_EQ( env.G[0], 0.0 );
-		ASSERT_DOUBLE_EQ( env.G[1], -g );
+		ASSERT_DOUBLE_EQ( env.G[OpenMps::AXIS_X], 0.0 );
+		ASSERT_DOUBLE_EQ( env.G[OpenMps::AXIS_Z], -g );
 		ASSERT_DOUBLE_EQ( env.Rho, rho );
 		ASSERT_DOUBLE_EQ( env.Nu, nu );
 		ASSERT_DOUBLE_EQ( env.SurfaceRatio, surfaceRatio );
-		ASSERT_DOUBLE_EQ( env.MinX[0], minX );
-		ASSERT_DOUBLE_EQ( env.MinX[1], minZ );
-		ASSERT_DOUBLE_EQ( env.MaxX[0], maxX );
-		ASSERT_DOUBLE_EQ( env.MaxX[1], maxZ );
+		ASSERT_DOUBLE_EQ( env.MinX[OpenMps::AXIS_X], minX );
+		ASSERT_DOUBLE_EQ( env.MinX[OpenMps::AXIS_Z], minZ );
+		ASSERT_DOUBLE_EQ( env.MaxX[OpenMps::AXIS_X], maxX );
+		ASSERT_DOUBLE_EQ( env.MaxX[OpenMps::AXIS_Z], maxZ );
 		ASSERT_GE( env.NeighborLength, r_eByl_0*l0 * (1+ courant*2) ); // 近傍粒子として保持する距離が、クーラン数による距離の二倍以上か？
 	}
 
-	}//openmps
+	// アクセッサが適切か/フィールドに設定されているか for Particles
+	TEST_F(DensityTest, FieldParticles){
+		auto particles = computer->Particles();
 
+	 // 粒子数チェック
+		ASSERT_EQ( particles.size(), num_ps_x*num_ps_z );
+
+		// 辺の長さが num_ps_x*l0, num_ps_z*l0 の長方形の内部に存在するか？
+		for(const auto& p : particles){
+			const double px = p.X()[OpenMps::AXIS_X];
+			const double pz = p.X()[OpenMps::AXIS_Z];
+			ASSERT_LE( px, (num_ps_x-1)*l0 );
+			ASSERT_GE( px, 0 );
+			ASSERT_LE( pz, (num_ps_z-1)*l0 );
+			ASSERT_GE( pz, 0 );
+		}
+	}
+
+
+	}//openmps
 }//anonymas
 
 	///#ifndef PRESSURE_EXPLICIT
