0fcc168f1116a186cfc08349d95e91a779a29090
maru10t <maru10tgg@gmail.com>
2019-11-01 16:43:01 +0900

    WIP, 行列ソルバテストのテストコード追加

--- src/OpenMps/test/test_ComputerImplicitForces.cpp
+++ src/OpenMps/test/test_ComputerImplicitForces.cpp
@@ -254,4 +254,129 @@ namespace {
 
 	}
 
+	TEST_F(ImplicitForcesTest, SolveIdentityMatrix)
+	{
+		std::vector<OpenMps::Particle> particles0;
+
+		for (auto j = decltype(num_z){0}; j < num_z; j++)
+		{
+			for (auto i = decltype(num_x){0}; i < num_x; i++)
+			{
+
+				auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
+				const double x = i * l0;
+				const double z = j * l0;
+				particle.X()[OpenMps::AXIS_X] = x;
+				particle.X()[OpenMps::AXIS_Z] = z;
+
+				particle.U()[OpenMps::AXIS_X] = 0.0;
+				particle.U()[OpenMps::AXIS_Z] = 0.0;
+				particle.P() = 0.0;
+				particle.N() = 0.0;
+
+				particles0.push_back(std::move(particle));
+			}
+		}
+		computer->AddParticles(std::move(particles0));
+
+		const auto& env = computer->GetEnvironment();
+		SearchNeighbor();
+		ComputeNeighborDensities();
+
+		SetPressurePoissonEquation();
+		auto& ppe = getPpe();
+		const auto Ndim = num_x * num_z;
+
+		for (auto j = decltype(Ndim){0}; j < Ndim; j++)
+		{
+			ppe.b(j) = static_cast<double>(j);
+			for (auto i = decltype(Ndim){0}; i < Ndim; i++)
+			{
+				if (i == j)
+				{
+					ppe.A(i, j) = 1.0;
+				} else {
+					ppe.A(i, j) = 0.0;
+				}
+			}
+		}
+
+		SolvePressurePoissonEquation();
+		double diff = 0.0;
+		for (auto i = decltype(Ndim){0}; i < Ndim; i++)
+		{
+			diff += abs(static_cast<double>(i) - ppe.x(i));
+		}
+
+		ASSERT_NEAR(diff / num_x, 0.0, 1e-5);
+	}
+
+		// 問題サイズを4x4として, 逆行列matrixを解く
+		TEST_F(ImplicitForcesTest, Solve4x4Matrix)
+		{
+			std::vector<OpenMps::Particle> particles0;
+
+			for (auto j = decltype(num_z){0}; j < 2; j++)
+			{
+				for (auto i = decltype(num_x){0}; i < 2; i++)
+				{
+
+					auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
+					const double x = i * l0;
+					const double z = j * l0;
+					particle.X()[OpenMps::AXIS_X] = x;
+					particle.X()[OpenMps::AXIS_Z] = z;
+
+					particle.U()[OpenMps::AXIS_X] = 0.0;
+					particle.U()[OpenMps::AXIS_Z] = 0.0;
+					particle.P() = 0.0;
+					particle.N() = 0.0;
+
+					particles0.push_back(std::move(particle));
+				}
+			}
+			computer->AddParticles(std::move(particles0));
+
+			const auto& env = computer->GetEnvironment();
+			SearchNeighbor();
+			ComputeNeighborDensities();
+
+			SetPressurePoissonEquation();
+			auto& ppe = getPpe();
+			const auto Ndim = 2 * 2;
+
+			ppe.A(0, 0) = 1.0;
+			ppe.A(0, 1) = 1.0;
+			ppe.A(0, 2) = 1.0;
+			ppe.A(0, 3) = -1.0;
+
+			ppe.A(1, 0) = 1.0;
+			ppe.A(1, 1) = 1.0;
+			ppe.A(1, 2) = -1.0;
+			ppe.A(1, 3) = 1.0;
+
+			ppe.A(2, 0) = 1.0;
+			ppe.A(2, 1) = -1.0;
+			ppe.A(2, 2) = 1.0;
+			ppe.A(2, 3) = 1.0;
+
+			ppe.A(3, 0) = -1.0;
+			ppe.A(3, 1) = 1.0;
+			ppe.A(3, 2) = 1.0;
+			ppe.A(3, 3) = 1.0;
+
+			ppe.b(0) = 4*1.0;
+			ppe.b(1) = 4*2.0;
+			ppe.b(2) = 4*3.0;
+			ppe.b(3) = 4*4.0;
+
+			SolvePressurePoissonEquation();
+
+			ASSERT_NEAR(ppe.x(0), 2.0, 1e-5);
+			ASSERT_NEAR(ppe.x(1), 4.0, 1e-5);
+			ASSERT_NEAR(ppe.x(2), 6.0, 1e-5);
+			ASSERT_NEAR(ppe.x(3), 8.0, 1e-5);
+		}
+	}
+
 }
