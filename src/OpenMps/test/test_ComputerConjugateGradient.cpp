e0a86ce609929c811281039b0bd66496b410c956
maru10t <maru10tgg@gmail.com>
2019-11-20 12:01:39 +0900

    行列求解をファイル分割

--- src/OpenMps/test/test_ComputerConjugateGradient.cpp
+++ src/OpenMps/test/test_ComputerConjugateGradient.cpp
@@ -1,6 +1,6 @@
-﻿#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-#define TEST_IMPLICITFORCES
+#define TEST_CONJUGATEGRADIENT
 #include "../Computer.hpp"
 #include "../Particle.hpp"
 #include "../Vector.hpp"
@@ -18,22 +18,22 @@ namespace {
 
 	static constexpr double rho = 998.2;
 	static constexpr double nu = 1.004e-06;
-	static constexpr double r_eByl_0 = 1.5; // テスト用の簡略化として, 小さめに設定.
+	static constexpr double r_eByl_0 = 1.5; // �e�X�g�p�̊ȗ����Ƃ���, �����߂ɐݒ�.
 
 #ifndef MPS_SPP
 	static constexpr double surfaceRatio = 0.95;
 #endif
-	// 格子状に配置する際の1辺あたりの粒子数
+	// �i�q��ɔz�u����ۂ�1�ӂ�����̗��q��
 	static constexpr std::size_t num_x = 7;
 	static constexpr std::size_t num_z = 7;
 
-	// サイズ上限を 横 2*l0*num_x, 縦 2*l0*num_z とする
+	// �T�C�Y����� �� 2*l0*num_x, �c 2*l0*num_z �Ƃ���
 	static constexpr double minX = -l0 * num_x;
 	static constexpr double minZ = -l0 * num_z;
 	static constexpr double maxX = l0 * num_x;
 	static constexpr double maxZ = l0 * num_z;
 
-	// 許容する相対誤差
+	// ���e���鑊�Ό덷
 	static constexpr double testAccuracy = 1e-3;
 
 
@@ -54,7 +54,7 @@ namespace {
 			return CreateVector(0.0, 0.0);
 		}
 
-		class ImplicitForcesTest : public ::testing::Test
+		class ConjugateGradientTest : public ::testing::Test
 		{
 		protected:
 			OpenMps::Computer<decltype(positionWall)&, decltype(positionWallPre)&>* computer;
@@ -166,8 +166,7 @@ namespace {
 			}
 		};
 
-		// 係数行列は対称行列であるか？
-		TEST_F(ImplicitForcesTest, MatrixSymmetry)
+		TEST_F(ConjugateGradientTest, SolveIdentityMatrix)
 		{
 			SearchNeighbor();
 			ComputeNeighborDensities();
@@ -176,46 +175,32 @@ namespace {
 			auto& ppe = getPpe();
 			const auto Ndim = num_x * num_z;
 
-			ASSERT_EQ(ppe.b.size(), Ndim);
-			for (auto i = decltype(Ndim){0}; i < Ndim; i++)
-			{
-				for (auto j = decltype(Ndim){0}; j < Ndim; j++)
-				{
-					if (j != i)
-					{
-						ASSERT_NEAR(ppe.A(i, j) - ppe.A(j, i), 0.0, testAccuracy);
-					}
-				}
-			}
-		}
-
-		// 係数行列 a_ii = -Σa_ij (i!=j) という恒等式は成立するか？
-		// 境界から離れた中央粒子においてテスト
-		TEST_F(ImplicitForcesTest, MatrixDiagIdentity)
-		{
-			SearchNeighbor();
-			ComputeNeighborDensities();
-			SetPressurePoissonEquation();
-
-			auto& ppe = getPpe();
-			const auto Ndim = num_x * num_z;
-
-			const auto ic = (num_x - 1) / 2 * (num_z + 1); // 中央粒子のindex
-
-			double sum_nondiag = 0.0;
 			for (auto j = decltype(Ndim){0}; j < Ndim; j++)
 			{
-				if (j != ic)
+				ppe.b(j) = static_cast<double>(j);
+				for (auto i = decltype(Ndim){0}; i < Ndim; i++)
 				{
-					sum_nondiag += ppe.A(ic, j); // disable,dummy,free surface particleは寄与しない
+					if (i == j)
+					{
+						ppe.A(i, j) = 1.0;
+					}
+					else {
+						ppe.A(i, j) = 0.0;
+					}
 				}
 			}
-			ASSERT_NEAR(ppe.A(ic, ic), -sum_nondiag, testAccuracy);
+
+			SolvePressurePoissonEquation();
+			double diff = 0.0;
+			for (auto i = decltype(Ndim){0}; i < Ndim; i++)
+			{
+				diff += abs(static_cast<double>(i) - ppe.x(i));
+			}
+
+			ASSERT_NEAR(diff / num_x, 0.0, 1e-5);
 		}
 
-		// 粒子i の 近傍粒子j に対応する成分が a_ij != 0 であること
-		// (dummy, disable, free surface粒子は除外)
-		TEST_F(ImplicitForcesTest, MatrixNeighborNonzero)
+		TEST_F(ConjugateGradientTest, Solve4x4Matrix)
 		{
 			SearchNeighbor();
 			ComputeNeighborDensities();
@@ -223,33 +208,52 @@ namespace {
 
 			auto& ppe = getPpe();
 			const auto Ndim = num_x * num_z;
-			const auto& particles = GetParticles();
 
-			for (auto i = decltype(Ndim){0}; i < Ndim; i++)
+			for (auto j = decltype(Ndim){0}; j < Ndim; j++)
 			{
-				if (!IsAlive(particles[i]))
+				ppe.b(j) = 0.0;
+				for (auto i = decltype(Ndim){0}; i < Ndim; i++)
 				{
-					continue;
-				}
-
-				std::vector<decltype(i)> neighList; // iの近傍粒子リスト
-				for (auto idx = decltype(i){0}; idx < NeighborCount(i); idx++)
-				{
-					const auto j = Neighbor(i, idx);
-					if (IsAlive(particles[j]))
+					if (i == j)
 					{
-						neighList.push_back(j);
-					}
-				}
-
-				for (auto j = decltype(Ndim){0}; j < Ndim; j++)
-				{
-					if (j != i && std::find(neighList.begin(), neighList.end(), j) == neighList.end()) // jがi近傍リストに属しない
-					{
-						ASSERT_NEAR(ppe.A(i, j), 0.0, testAccuracy);
+						ppe.A(i, j) = 1.0;
+					} else {
+						ppe.A(i, j) = 0.0;
 					}
 				}
 			}
+
+			ppe.A(0, 0) = 1.0;
+			ppe.A(0, 1) = 1.0;
+			ppe.A(0, 2) = 1.0;
+			ppe.A(0, 3) = -1.0;
+
+			ppe.A(1, 0) = 1.0;
+			ppe.A(1, 1) = 1.0;
+			ppe.A(1, 2) = -1.0;
+			ppe.A(1, 3) = 1.0;
+
+			ppe.A(2, 0) = 1.0;
+			ppe.A(2, 1) = -1.0;
+			ppe.A(2, 2) = 1.0;
+			ppe.A(2, 3) = 1.0;
+
+			ppe.A(3, 0) = -1.0;
+			ppe.A(3, 1) = 1.0;
+			ppe.A(3, 2) = 1.0;
+			ppe.A(3, 3) = 1.0;
+
+			ppe.b(0) = 4*1.0;
+			ppe.b(1) = 4*2.0;
+			ppe.b(2) = 4*3.0;
+			ppe.b(3) = 4*4.0;
+
+			SolvePressurePoissonEquation();
+
+			ASSERT_NEAR(ppe.x(0), 2.0, 1e-5);
+			ASSERT_NEAR(ppe.x(1), 4.0, 1e-5);
+			ASSERT_NEAR(ppe.x(2), 6.0, 1e-5);
+			ASSERT_NEAR(ppe.x(3), 8.0, 1e-5);
 		}
 	}
 
