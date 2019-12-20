f171aa3653a1b8ea68f3f93e39fe3bcfefc10880
maru10t <maru10tgg@gmail.com>
2019-12-20 16:02:52 +0900

    中央での値比較 -> 分布の比較

--- src/OpenMps/test/test_ComputerModifyByPressureGradient.cpp
+++ src/OpenMps/test/test_ComputerModifyByPressureGradient.cpp
@@ -20,10 +20,10 @@ namespace {
 #ifndef MPS_SPP
 	static constexpr double surfaceRatio = 0.95;
 #endif
-	static constexpr double minX = -10.0*l0;
-	static constexpr double minZ = -10.0*l0;
-	static constexpr double maxX = 10.0 * l0;
-	static constexpr double maxZ = 10.0 * l0;
+	static constexpr double minX = -20.0*l0;
+	static constexpr double minZ = -20.0*l0;
+	static constexpr double maxX = 20.0 * l0;
+	static constexpr double maxZ = 20.0 * l0;
 
 #ifdef PRESSURE_EXPLICIT
 	static constexpr double c = 1.0;
@@ -107,24 +107,27 @@ namespace OpenMps
 		}
 	};
 
-	TEST_F(PressureGradientTest, GradValue)
+	TEST_F(PressureGradientTest, CompareGradientProfile)
 	{
 		std::vector<OpenMps::Particle> particles;
-		static constexpr std::size_t num_x = 5;
-		static constexpr std::size_t num_z = 5;
-		constexpr auto gradp = 10.0;
+		static constexpr std::size_t num_x = 15;
+		static constexpr std::size_t num_z = 15;
+		constexpr auto dpx = 12.3;
+		constexpr auto waveNumZ = 2.0*M_PI/num_z/l0;
 
 		for (auto j = decltype(num_z){0}; j < num_z; j++)
 		{
 			for (auto i = decltype(num_x){0}; i < num_x; i++)
 			{
 				auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
-				particle.X()[OpenMps::AXIS_X] = i * l0;
-				particle.X()[OpenMps::AXIS_Z] = j * l0;
+				const double x = i * l0;
+				const double z = j * l0;
+				particle.X()[OpenMps::AXIS_X] = x;
+				particle.X()[OpenMps::AXIS_Z] = z;
 
 				particle.U()[OpenMps::AXIS_X] = 0.0;
 				particle.U()[OpenMps::AXIS_Z] = 0.0;
-				particle.P() = j*gradp;
+				particle.P() = 0.5 * dpx * x * x + sin(waveNumZ * z);
 				particle.N() = 0.0;
 
 				particles.push_back(std::move(particle));
@@ -137,20 +140,29 @@ namespace OpenMps
 
 		ModifyByPressureGradientTest();
 
-		auto p = GetParticles();
-		auto env = GetEnvironment();
-		const auto prefact = (-env.Dt())/env.Rho;
-		constexpr auto id = (num_x - 1) / 2 * (num_z + 1);
-		const double du = p[id].U()[OpenMps::AXIS_X];
-		const double dv = p[id].U()[OpenMps::AXIS_Z];
-		const double dx = p[id].X()[OpenMps::AXIS_X] - (num_z-1)/2*l0;
-		const double dz = p[id].X()[OpenMps::AXIS_Z] - (num_z-1)/2*l0;
+		const auto p = GetParticles();
+		const auto env = GetEnvironment();
+		const auto prefact = -env.Dt() / env.Rho;
+		const auto blankWidth = 3;
 
-		ASSERT_NEAR(du, 0.0, testAccuracy);
-		ASSERT_NEAR(dv, gradp*prefact, testAccuracy);
-		ASSERT_NEAR(dx, 0.0, testAccuracy);
-		ASSERT_NEAR(dz, gradp*prefact*env.Dt(), testAccuracy);
+		for (auto j = decltype(num_z){blankWidth}; j < num_z - blankWidth; j++)
+		{
+			for (auto i = decltype(num_x){3}; i < num_x - 3; i++)
+			{
+				const auto id = i + j * num_x;
+				const double du = p[id].U()[OpenMps::AXIS_X];
+				const double dv = p[id].U()[OpenMps::AXIS_Z];
+				const double dx = p[id].X()[OpenMps::AXIS_X] - i * l0;
+				const double dz = p[id].X()[OpenMps::AXIS_Z] - j * l0;
+
+				const double x = p[id].X()[OpenMps::AXIS_X];
+				const double z = p[id].X()[OpenMps::AXIS_Z];
+				ASSERT_NEAR(du, dpx * x * prefact, testAccuracy);
+				ASSERT_NEAR(dv, waveNumZ * cos(waveNumZ * z) * prefact, testAccuracy);
+				ASSERT_NEAR(dx, du * env.Dt(), testAccuracy);
+				ASSERT_NEAR(dz, dv * env.Dt(), testAccuracy);
+			}
+		}
 	}
-
 }
 }
