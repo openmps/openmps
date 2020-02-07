16f602bf9270c648b4fce3618ebe6937ad3f598b
maru10t <maru10tgg@gmail.com>
2020-02-07 17:07:50 +0900

    速度場が多項式である数密度変化テストケースを追加

--- src/OpenMps/test/test_ComputerNeighborDensityVariation.cpp
+++ src/OpenMps/test/test_ComputerNeighborDensityVariation.cpp
@@ -15,11 +15,11 @@ namespace {
 	static constexpr double courant = 0.1;
 
 	static constexpr double l0 = 1.0;
-	static constexpr double g = 9.8;
+	static constexpr double g = 0.0;
 
 	static constexpr double rho = 998.2;
 	static constexpr double nu = 1.004e-06;
-	static constexpr double r_eByl_0 = 1.5;
+	static constexpr double r_eByl_0 = 5.0;
 #ifndef MPS_SPP
 	static constexpr double surfaceRatio = 0.95;
 #endif
@@ -32,10 +32,10 @@ namespace {
 	static constexpr double c = 1.0;
 #endif
 
-	static constexpr double testAccuracy = 0.05;
-	static constexpr std::size_t num_x = 20;
-	static constexpr std::size_t num_z = 20;
-	static constexpr std::size_t wallMargin = 5;
+	static constexpr double testAccuracy = 0.10;
+	static constexpr std::size_t num_x = 35;
+	static constexpr std::size_t num_z = 35;
+	static constexpr std::size_t wallMargin = 15;
 
 	namespace OpenMps
 	{
@@ -75,10 +75,10 @@ namespace {
 
 				computer = new OpenMps::Computer<decltype(positionWall)&, decltype(positionWallPre)&>(
 #ifndef PRESSURE_EXPLICIT
-						eps,
+					eps,
 #endif
-						environment,
-						positionWall, positionWallPre);
+					environment,
+					positionWall, positionWallPre);
 			}
 
 			auto& GetEnvironment()
@@ -101,41 +101,41 @@ namespace {
 				computer->ComputeNeighborDensities();
 			}
 
-			void ModifyByPressureGradientTest()
-			{
-				computer->ModifyByPressureGradient();
-			}
-
 			virtual void TearDown()
 			{
 				delete computer;
 			}
+
+			auto NeighborDensityVariationSpeed(const std::size_t i)
+			{
+				return computer->NeighborDensityVariationSpeed(i);
+			}
 		};
 
-		TEST_F(NeighborDensityTest, ValueTest)
+		// 粒子は等間隔l0の格子上(num_x,num_z)に配置
+		// (u,v) = (ax,bz) 型の、発散が -(a+b) である速度場を与える
+		TEST_F(NeighborDensityTest, ValueTestLinear)
 		{
 			std::vector<OpenMps::Particle> particles;
 
-			// dlは分布関数の空間変化スケール
-			// 最小長さである粒子相互作用半径の数倍に設定
-			// 分布関数の空間変化が激しすぎて解像度限界を超えないようにしている
-			static constexpr auto dl = r_eByl_0 * 10.0;
-			static constexpr auto gradpx = 1.0 / (dl*dl);
-			static constexpr auto gradpz = -1.0 / (dl*dl);
+			static constexpr auto gradvx = 1.0;
+			static constexpr auto gradvz = -0.3;
+
+			auto env = GetEnvironment();
 
 			for (auto j = decltype(num_z){0}; j < num_z; j++)
 			{
 				for (auto i = decltype(num_x){0}; i < num_x; i++)
 				{
 					auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
-					const auto xij = static_cast<double>(i) * l0;
-					const auto zij = static_cast<double>(j) * l0;
+					const auto xij = static_cast<double>(i)* l0;
+					const auto zij = static_cast<double>(j)* l0;
 					particle.X()[OpenMps::AXIS_X] = xij;
 					particle.X()[OpenMps::AXIS_Z] = zij;
 
-					particle.U()[OpenMps::AXIS_X] = 0.0;
-					particle.U()[OpenMps::AXIS_Z] = 0.0;
-					particle.P() = 1.0/2.0 * (xij*xij) * gradpx + 1.0/3.0 * (zij*zij*zij) * gradpz;
+					particle.U()[OpenMps::AXIS_X] = gradvx * xij;
+					particle.U()[OpenMps::AXIS_Z] = gradvz * zij;
+					particle.P() = 0.0;
 					particle.N() = 0.0;
 
 					particles.push_back(std::move(particle));
@@ -146,40 +146,74 @@ namespace {
 			SearchNeighbor();
 			ComputeNeighborDensities();
 
-			ModifyByPressureGradientTest();
-
 			auto p = GetParticles();
-			auto env = GetEnvironment();
 			const auto prefact = (-env.Dt()) / env.Rho;
-			
-			// 壁から離れた部分で圧力勾配を解析解と比較
-			for (auto j = decltype(num_z){wallMargin}; j < num_z-wallMargin; j++)
+
+			// wallMargin だけ離れた、中心付近において数値解と解析解を比較
+			for (auto j = decltype(num_z){wallMargin}; j < num_z - wallMargin; j++)
 			{
-				for (auto i = decltype(num_x){wallMargin}; i < num_x-wallMargin; i++)
+				for (auto i = decltype(num_x){wallMargin}; i < num_x - wallMargin; i++)
 				{
 					const auto id = i + num_x * j;
-					const auto xij0 = static_cast<double>(i) * l0;
-					const auto zij0 = static_cast<double>(j) * l0;
-					const auto xij = p[id].X()[OpenMps::AXIS_X];
-					const auto zij = p[id].X()[OpenMps::AXIS_Z];
+					auto dndt = NeighborDensityVariationSpeed(id) / p[i].N();
+					auto dndt_analy = -(gradvx + gradvz);
 
-					const double du = p[id].U()[OpenMps::AXIS_X];
-					const double dv = p[id].U()[OpenMps::AXIS_Z];
-					const double dx = xij - xij0;
-					const double dz = zij - zij0;
-
-					const double dpx_analy = gradpx * xij0;
-					const double dpz_analy = gradpz * zij0 * zij0;
-					const double dx_analy = prefact * dpx_analy * env.Dt(); 
-					const double dz_analy = prefact * dpz_analy * env.Dt(); 
-
-					ASSERT_NEAR(abs((du/prefact - dpx_analy) / dpx_analy), 0.0, testAccuracy);
-					ASSERT_NEAR(abs((dv/prefact - dpz_analy) / dpz_analy), 0.0, testAccuracy);
-					ASSERT_NEAR(abs((dx - dx_analy) / dx_analy), 0.0, testAccuracy);
-					ASSERT_NEAR(abs((dz - dz_analy) / dz_analy), 0.0, testAccuracy);
+					ASSERT_NEAR(abs((dndt - dndt_analy) / dndt_analy), 0.0, testAccuracy);
 				}
 			}
 		}
 
+		// 粒子は等間隔l0の格子上(num_x,num_z)に配置
+		// (u,v) = (ax^2,bz^3) 型の、発散が -(2ax+3bz) である速度場を与える
+		TEST_F(NeighborDensityTest, ValueTestPolynomial)
+		{
+			std::vector<OpenMps::Particle> particles;
+
+			static constexpr auto gradvx = 1.0;
+			static constexpr auto gradvz = -1.5;
+
+			auto env = GetEnvironment();
+
+			for (auto j = decltype(num_z){0}; j < num_z; j++)
+			{
+				for (auto i = decltype(num_x){0}; i < num_x; i++)
+				{
+					auto particle = OpenMps::Particle(OpenMps::Particle::Type::IncompressibleNewton);
+					const auto xij = static_cast<double>(i)* l0;
+					const auto zij = static_cast<double>(j)* l0;
+					particle.X()[OpenMps::AXIS_X] = xij;
+					particle.X()[OpenMps::AXIS_Z] = zij;
+
+					particle.U()[OpenMps::AXIS_X] = 1.0 / 2.0 * gradvx * (xij * xij);
+					particle.U()[OpenMps::AXIS_Z] = 1.0 / 3.0 * gradvz * (zij * zij * zij);
+					particle.P() = 0.0;
+					particle.N() = 0.0;
+
+					particles.push_back(std::move(particle));
+				}
+			}
+			computer->AddParticles(std::move(particles));
+
+			SearchNeighbor();
+			ComputeNeighborDensities();
+
+			auto p = GetParticles();
+			const auto prefact = (-env.Dt()) / env.Rho;
+
+			for (auto j = decltype(num_z){wallMargin}; j < num_z - wallMargin; j++)
+			{
+				for (auto i = decltype(num_x){wallMargin}; i < num_x - wallMargin; i++)
+				{
+					const auto id = i + num_x * j;
+					const auto xij = static_cast<double>(i)* l0;
+					const auto zij = static_cast<double>(j)* l0;
+
+					auto dndt = NeighborDensityVariationSpeed(id) / p[i].N();
+					auto dndt_analy = -(gradvx * xij + gradvz * zij * zij);
+
+					ASSERT_NEAR(abs((dndt - dndt_analy) / dndt_analy), 0.0, testAccuracy);
+				}
+			}
+		}
 	}
 }
